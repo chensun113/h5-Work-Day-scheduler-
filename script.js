909814afb1371612832975dee6a5a9dccbfc22e8
chensun113 <59670255+chensun113@users.noreply.github.com>
2020-11-02 16:49:11 -0500

    Update script.js
    I wrote js codes in html.(wrong file)

--- script.js
+++ script.js
@@ -1,40 +1,67 @@
+//scroll down
+window.onscroll = function() {scrollFunction()};
+function scrollFunction(){
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("cc").style.top = "0";
+      } else {
+        document.getElementById("cc").style.top = "-50px";
+      }
+}
 //Show Time
 var t = document.querySelector("#currentDay");
 var t2 = moment().format('LLLL');
-t.textContent=t2;
+t.textContent = t2;
 //grey part
-var rowtext =["9AM","10AM","11AM"];
+var rowtext = ["9AM  ", "10AM", "11AM"];
 var todolist = document.getElementById("cc");//timelist;
 grey();
-function grey(){
-   // var todolist = document.getElementById("cc");//timelist;
+function grey() {
+    // var todolist = document.getElementById("cc");//timelist;
 
-   
-   for(var i = 0; i < 3; i++){
+
+    for (var i = 0; i < 3; i++) {
+        //whole content
+        var div_time_block = document.createElement("div");
+        div_time_block.setAttribute("class", "time-block");
        
-        var textarea = document.createElement("textarea");//中心title
-        // textarea.contains="1234";
+        //line
+        var div_hour = document.createElement("div");
+        div_hour.setAttribute("class", "hour");
+       //white part with time title: 9AM,10AM,11AM;
+       var div_row = document.createElement("div");
+        div_row.setAttribute("class", "row");
+        var todo = rowtext[i];
+        div_row.textContent = todo;
+       
+        var div_past = document.createElement("div");
+        div_past.setAttribute("class", "past");
+        //grey textareas
+        var textarea = document.createElement("textarea");
+        var size1 = 110;
         textarea.setAttribute("name", "textarea");
-        textarea.setAttribute("id", "textarea");
-        textarea.setAttribute("col", "110");
-        textarea.setAttribute("row", "3");
-
-        var divpast =document.createElement("div");//外一层title
-        divpast.appendChild(textarea);
-        divpast.setAttribute("class","past");
-
-        var Row = document.createElement("div");
-        Row.appendChild(divpast);
-        Row.setAttribute("class","row");
-
-     
-
-
-        // todolist.appendChild(Row);
-        todolist.appendChild(divpast);
+        textarea.setAttribute("cols", size1);
+        //blue part(white)
+        var div_saveBtn = document.createElement("div");
+        div_saveBtn.textContent = "                ";
+        div_saveBtn.setAttribute("class", "saveBtn");
     
-    
-    }
+        //blue button
+        var div_description = document.createElement("button");
+        div_description.setAttribute("class", "description");
+        // div_description.setAttribute(div_description, size2);
+        div_description.textContent=" ";
+        todolist.appendChild(div_time_block);
 
+        div_time_block.appendChild(div_hour);
+        div_time_block.appendChild(div_row);
 
-}
+        div_row.appendChild(div_past);
+        div_row.appendChild(div_saveBtn);
+
+        div_past.appendChild(textarea);
+
+        div_saveBtn.appendChild(div_description);
+    }//for-loop-3-grey
+}//function grey();
+
